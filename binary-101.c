72af0ec28e691758ce76b14467d558890294ed8c
Minh Thong <dominhthong99@gmail.com>
2019-09-09 20:52:51 +0700

    hw2

---
+++ binary-101.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int x[100];
+int n;
+
+int check(int v, int k) {
+    if(v == 1 && x[k - 1] == 0 && x[k - 2] == 1)
+	    return 0;
+    return 1;
+}
+
+void solution () {
+	int i;
+	for (i = 1; i <= n; i++) {
+		printf("%d", x[i]);
+    }
+	printf ("\n");
+}
+
+void TRY(int k) {
+	int v;
+	for(v = 0; v <= 1; v++) {
+		if(check (v, k)) {
+			x[k] = v;
+			if(k == n) solution ();
+			else TRY(k + 1);
+		}
+	}
+}
+
+int main () {
+	n = 5;
+	TRY(1);
+	return 0;
+}
