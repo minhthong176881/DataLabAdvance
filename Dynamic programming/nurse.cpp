6a213beb682e1dfbbc234aaa9359b33c083406c4
minhthong176881 <dominhthong99@gmail.com>
2019-10-25 11:28:44 +0700

    update nurse.cpp

--- Dynamic programming/nurse.cpp
+++ Dynamic programming/nurse.cpp
@@ -4,4 +4,36 @@
 #define MOD 100000007
 using namespace std;
 
+int n, k1, k2;
+long long m;
+int s[maxN];
+int s1[maxN];
 
+void input(){
+    cin >> n >> k1 >> k2;
+}
+
+int solve(){
+    int i, l;
+    for(i = 1; i <= n; i++){
+        s[i] = 0;
+        s1[i] = 0;
+    }
+    s[1] = 1; 
+    s[0] = 1;
+    s1[k1] = 1;
+    for(int i = k1 + 1; i <= n; i++){
+        s[i] = s1[i - 1];
+        s1[i] =0;
+        for(l = k1; l <= k2; l++){
+            if(i - l >= 0) s1[i] = s1[i] % MOD + s[i - l] % MOD;
+        }
+    }
+    return s[n] % MOD + s1[n] % MOD;
+}
+
+int main(){
+    input();
+    cout << solve() << endl;
+    return 0;
+}
